5da2b3dac07edb64facffc765556c10e2c7c291f
Younixue3 <bussiness.needs.rctka@gmail.com>
2023-03-27 08:25:39 +0800

    Fixing

--- storage/views.py
+++ storage/views.py
@@ -6,9 +6,16 @@ from rest_framework.response import Response
 
 @api_view(['POST'])
 def store_file(request):
-    print(request.data)
-    file = request.FILES['bukti_pembayaran']
-    fs = FileSystemStorage()
-    filename = fs.save(file.name, file)
-    uploaded_file_url = fs.url(filename)
+    print(request.FILES)
+    if request.query_params['single'] == 'true':
+        file = request.FILES['image']
+        fs = FileSystemStorage()
+        filename = fs.save(file.name, file)
+        uploaded_file_url = fs.url(filename)
+    else:
+        file = request.FILES['bukti_pembayaran']
+        fs = FileSystemStorage()
+        filename = fs.save(file.name, file)
+        uploaded_file_url = fs.url(filename)
     return Response(request.build_absolute_uri(uploaded_file_url))
+
